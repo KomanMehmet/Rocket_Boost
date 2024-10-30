11a98d29450393fa99a6885f6864a1b76d2a8448
KomanMehmet <108063848+KomanMehmet@users.noreply.github.com>
2024-10-30 19:54:15 +0300

    Rocket_Thrust_And_Rotation

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,60 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private InputAction _thrust;
+    [SerializeField] private InputAction _rotation;
+    [SerializeField] private float _thrustStrength = 100f;
+    [SerializeField] private float _rotationStrength = 100f;
+    
+    private Rigidbody rocketRb;
+
+    private void OnEnable()
+    {
+        _thrust.Enable();
+        _rotation.Enable();
+    }
+
+    private void Start()
+    {
+        rocketRb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    private void ProcessThrust()
+    {
+        if (_thrust.IsPressed())
+        {
+            rocketRb.AddRelativeForce(Vector3.up * _thrustStrength * Time.fixedDeltaTime);
+        }
+    }
+
+    private void ProcessRotation()
+    {
+        float rotationInput = _rotation.ReadValue<float>();
+
+        if (rotationInput < 0f)
+        {
+            ApplyRotation(_rotationStrength);
+        }
+        else if (rotationInput > 0f)
+        {
+            ApplyRotation(-_rotationStrength);
+        }
+    }
+
+    private void ApplyRotation(float rotationStrength)
+    {
+        rocketRb.freezeRotation = true;
+        transform.Rotate(Vector3.forward * rotationStrength * Time.fixedDeltaTime);
+        rocketRb.freezeRotation = false;
+
+    }
+}
